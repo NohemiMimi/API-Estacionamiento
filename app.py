e3e9b78d28386d350e976dba15545fa923883f48
Nohemi <159970612+NohemiMimi@users.noreply.github.com>
2026-03-06 14:56:11 -0600

    API

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
